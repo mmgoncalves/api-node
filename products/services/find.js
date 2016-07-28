7dfda3d9aa0fed330c41d784d24dcbae6c5ff667
Mateus Marques <marques.m05@gmail.com>
2016-07-28 16:44:04 -0300

    att

--- products/services/find.js
+++ products/services/find.js
@@ -1,5 +1,11 @@
+var xml2js = require('xml2js').parseString;
 var Service = function(req, res){
-    res.send('OK');
+    var xml = '<note><script id="tinyhippos-injected"/><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>';
+
+    xml2js(xml, function(err, result){
+        res.status(200)
+            .send(result);
+    });
 };
 
 module.exports = Service;
