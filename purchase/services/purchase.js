5459f84ea068e1ab6b27f2f33c5ea7a915f394a1
Mateus Marques <marques.m05@live.com>
2016-07-31 00:52:08 -0300

    finalizando o carrinho de compras

---
+++ purchase/services/purchase.js
@@ -0,0 +1,68 @@
+var Purchase = require('./../entity/purchase');
+var Shopping = require('./../../shopping/entity/shopping');
+
+var Service = function(req, res, next){
+    var purchase = Purchase(req.body);
+    purchase.shopping = req.params.shoppingId;
+
+    Shopping
+        .findById(req.params.shoppingId)
+        .populate('products')
+        .exec()
+        .then(function(shopping){
+            if(!shopping){
+                return res.status(404)
+                    .json({
+                        status: false,
+                        data: {}
+                    });
+            }
+
+            var total = 0;
+            shopping.products.forEach(function(value, key){
+                if((shopping.products.length - 1) !== key){
+                    total = total + value.price;
+                }
+
+                if(shopping.products.length === 1){
+                    total = shopping.products[key].price;
+                }
+
+                purchase.total = total;
+
+                purchase
+                    .save()
+                    .then(function(saved){
+                        return res.status(200)
+                            .json({
+                                status: true,
+                                data: saved
+                            })
+                    })
+                    .catch(function(err){
+                        return res.status(500)
+                            .json({
+                                status: false,
+                                data: {error: err}
+                            });
+                    });
+            });
+
+            /*
+            return res.status(200)
+                .json({
+                    status: true,
+                    data: shopping
+                });
+                */
+        })
+        .catch(function(err){
+            return res.status(500)
+                .json({
+                    status:false,
+                    data: {error: err}
+                });
+        });
+};
+
+module.exports = Service;
